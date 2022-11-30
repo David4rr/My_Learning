21c640a98e4a02719104b4628cf2fe1d1efe26da
David <davidarrozaqi085@gmail.com>
2022-11-30 13:28:13 +0700

    Date Picker

---
+++ date_picker/lib/main.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: camel_case_types, avoid_print
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      restorationScopeId: 'app',
+      debugShowCheckedModeBanner: false,
+      home: tanggal(restorationId: 'main'),
+    );
+  }
+}
+
+class tanggal extends StatefulWidget {
+  const tanggal({super.key, this.restorationId});
+
+  final String? restorationId;
+
+  @override
+  State<tanggal> createState() => _tanggalState();
+}
+
+class _tanggalState extends State<tanggal> with RestorationMixin {
+  @override
+  String? get restorationId => widget.restorationId;
+
+  final RestorableDateTime _selectedDate = RestorableDateTime(DateTime.now());
+  late final RestorableRouteFuture<DateTime?> _datePicker =
+      RestorableRouteFuture<DateTime?>(
+    onComplete: _selectDate,
+    onPresent: (NavigatorState navigator, Object? arguments) {
+      return navigator.restorablePush(
+        _datePickerRoute,
+        arguments: _selectedDate.value.millisecondsSinceEpoch,
+      );
+    },
+  );
+
+  static Route<DateTime> _datePickerRoute(
+    BuildContext context,
+    Object? arguments,
+  ) {
+    return DialogRoute<DateTime>(
+      context: context,
+      builder: (BuildContext context) {
+        return DatePickerDialog(
+          restorationId: 'date_picker_dialog',
+          initialEntryMode: DatePickerEntryMode.calendarOnly,
+          initialDate: DateTime.fromMillisecondsSinceEpoch(arguments! as int),
+          firstDate: DateTime(2000),
+          lastDate: DateTime(2023),
+        );
+      },
+    );
+  }
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool intialRestore) {
+    registerForRestoration(_selectedDate, 'selected_date');
+    registerForRestoration(_datePicker, 'date_picker_route_future');
+  }
+
+  void _selectDate(DateTime? newSelectedDate) {
+    if (newSelectedDate != null) {
+      setState(() {
+        _selectedDate.value = newSelectedDate;
+        print(_selectedDate.value);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Date Picker"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            setState(
+              () {
+                _datePicker.present();
+              },
+            );
+          },
+          child: const Text("Date Picker"),
+        ),
+      ),
+    );
+  }
+}
