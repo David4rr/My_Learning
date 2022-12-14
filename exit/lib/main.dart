581485716ffc4a87eea6a8b410af1b5834223092
David <davidarrozaqi085@gmail.com>
2022-12-14 20:20:51 +0700

    add willpopscope

---
+++ exit/lib/main.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Exit",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future<bool> showExitPopup() async {
+      return await showDialog(
+            //show confirm dialogue
+            //the return value will be from "Yes" or "No" options
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Exit App'),
+              content: const Text('Do you want to exit an App?'),
+              actions: [
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  //return false when click on "NO"
+                  child: const Text('No'),
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  //return true when click on "Yes"
+                  child: const Text('Yes'),
+                ),
+              ],
+            ),
+          ) ??
+          false; //if showDialouge had returned null, then return false
+    }
+
+    return WillPopScope(
+        onWillPop: showExitPopup, //call function on back button press
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text("Override Back Button"),
+              backgroundColor: Colors.redAccent,
+            ),
+            body: const Center(
+              child: Text("Override Back Buttton"),
+            )));
+  }
+}
